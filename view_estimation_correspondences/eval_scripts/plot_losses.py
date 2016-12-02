3f0954f4ecaa41517ca415fba5c3a677debaea41
Ryan Szeto <szetor@umich.edu>
2016-12-02 09:21:51 -0500

    Corrected loss plotting code

--- view_estimation_correspondences/eval_scripts/plot_losses.py
+++ view_estimation_correspondences/eval_scripts/plot_losses.py
@@ -22,7 +22,7 @@ def plotLosses(args):
         trainTable = np.hstack((trainTable, totalLoss))
         # Add total loss to header
         header.append('Loss_Total')
-    
+
     # Plot training loss curves
     plt.figure()
     ax = plt.subplot(111)
@@ -34,47 +34,70 @@ def plotLosses(args):
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     # Add labels
-    ax.set_title('Training loss vs. iterations')
+    ax.set_title(savePath + '\nTraining loss vs. iterations')
     ax.set_xlabel('Number of iterations')
     ax.set_ylabel('Loss')
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     # Save figure if specified
     if savePath:
         plt.savefig(os.path.join(savePath, 'trainLoss.png'))
-    
+
     # Extract information from testing table
     # Note: Last row may be incomplete. Blank values are nan
     with open(testLog) as f:
         splitLines = [line.split() for line in f.readlines()]
         header = splitLines[0]
         dataAsStr = splitLines[1:]
-        trainTable = np.full((len(dataAsStr), len(header)), np.nan)
+        testTable = np.full((len(dataAsStr), len(header)), np.nan)
         for i,line in enumerate(dataAsStr):
-            trainTable[i, :len(line)] = line
-        # Append total loss as last column
-        totalLoss = np.sum(trainTable[:, 5:], axis=1).reshape((trainTable.shape[0]), 1)
-        trainTable = np.hstack((trainTable, totalLoss))
+            testTable[i, :len(line)] = line
+        # Append total loss
+        totalLoss = np.sum(testTable[:, 5:8], axis=1).reshape(testTable.shape[0], 1)
+        testTable = np.hstack((testTable, totalLoss))
         # Add total loss to header
         header.append('Loss_Total')
+        # Append total acc
+        totalAcc = np.sum(testTable[:, 2:5], axis=1).reshape(testTable.shape[0], 1)
+        testTable = np.hstack((testTable, totalAcc))
+        header.append('Acc_Total')
     
-    # Plot training loss curves
+    # Plot testing loss curves
     plt.figure()
     ax = plt.subplot(111)
-    ax.plot(trainTable[:, 0], trainTable[:, 5], color='g', label='Azimuth')
-    ax.plot(trainTable[:, 0], trainTable[:, 6], color='r', label='Elevation')
-    ax.plot(trainTable[:, 0], trainTable[:, 7], color='c', label='Tilt')
-    ax.plot(trainTable[:, 0], trainTable[:, 8], color='b', label='Total')
+    ax.plot(testTable[:, 0], testTable[:, 5], color='g', label='Azimuth')
+    ax.plot(testTable[:, 0], testTable[:, 6], color='r', label='Elevation')
+    ax.plot(testTable[:, 0], testTable[:, 7], color='c', label='Tilt')
+    ax.plot(testTable[:, 0], testTable[:, 8], color='b', label='Total')
     # Adjust graph so legend appears to the right of the plot
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     # Add labels
-    ax.set_title('Testing loss vs. iterations')
+    ax.set_title(savePath + '\nTesting loss vs. iterations')
     ax.set_xlabel('Number of iterations')
     ax.set_ylabel('Loss')
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     # Save figure if specified
     if savePath:
         plt.savefig(os.path.join(savePath, 'testLoss.png'))
+
+    # Plot testing acc curves
+    plt.figure()
+    ax = plt.subplot(111)
+    ax.plot(testTable[:, 0], testTable[:, 2], color='g', label='Azimuth')
+    ax.plot(testTable[:, 0], testTable[:, 3], color='r', label='Elevation')
+    ax.plot(testTable[:, 0], testTable[:, 4], color='c', label='Tilt')
+    ax.plot(testTable[:, 0], testTable[:, 9], color='b', label='Total')
+    # Adjust graph so legend appears to the right of the plot
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    # Add labels
+    ax.set_title(savePath + '\nTesting accuracy vs. iterations')
+    ax.set_xlabel('Number of iterations')
+    ax.set_ylabel('Loss')
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    # Save figure if specified
+    if savePath:
+        plt.savefig(os.path.join(savePath, 'testAcc.png'))
 
     # Show plots if specified
     if args.showPlots:
